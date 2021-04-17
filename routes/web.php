90155e85162365d069f39a0132f96b28b1a91e96
rifjan29 <rjndla@gmail.com>
2021-04-17 14:31:10 +0800

    Migration

--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::group(['namespace' => 'Frontend'], function(){
-    Route::resource('/', 'FrontendController');
-});
-Route::group(['namespace' => 'Backend'], function(){
-    Route::resource('dashboard', 'BackendController');
+Route::get('/', function () {
+    return view('welcome');
 });
